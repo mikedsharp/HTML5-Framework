0c26a063f2ce0e6bf8081b953650d7fc53596f4d
programmerinprogress <msharp23@hotmail.co.uk>
2014-05-04 12:03:01 +0100

    Z-layering and event propagation Yo!
    Just a little sneak peak at what I'm playing around with, I will roll
    all of this into a neat little interface for sprites and other objects,
    but just playing around with it at the moment test the concept.

--- scripts/game.state.defaultstate.js
+++ scripts/game.state.defaultstate.js
@@ -1,83 +1,86 @@
 ﻿game.state.defaultstate = (function () {
-	var my = {
-		xValue: 320,
-		yValue: 240,
+    var my = {
+        xValue: 320,
+        yValue: 240,
         once: false
-	};
+    };
 
-	my.init = function () {
-	    addEventListener('click', my.checkMouseCoordinates);
-	    addEventListener('resize', my.refreshEvents)
-	    
-	}
+    my.init = function () {
+        addEventListener('click', my.checkMouseCoordinates);
+        addEventListener('resize', my.refreshEvents)
 
-	my.refreshEvents = function () {
-	    my.once = false;
-	    system.event.mousemonitor.subscribers = []; 
-	}
-    
-	my.handleInput = function () {
+    }
 
-		if (system.event.keymonitor.keyStates.right) {
-			my.xValue += 5; 
-		}
-		if (system.event.keymonitor.keyStates.left) {
-			my.xValue -= 5;
-		}
-		if (system.event.keymonitor.keyStates.down) {
-			my.yValue += 5;
-		}
-		if (system.event.keymonitor.keyStates.up) {
-			my.yValue -= 5;
-		}
-		if (system.event.keymonitor.keyStates.spaceBar) {
-		    console.log('spacebar pushed');
-		}
-		if (system.event.keymonitor.keyStates.enter) {
-		    console.log('enter pushed');
-		}
+    my.refreshEvents = function () {
+        my.once = false;
+        system.event.mousemonitor.subscribers = [];
+    }
 
-	}
+    my.handleInput = function () {
 
-	my.checkMouseCoordinates = function (event) {
-	    //console.log('X: ' + event.pageX, 'Y: ' + event.pageY); 
-	}
-	my.clickHandler = function (object) {
-	    console.log('X: ' + object.x +' Y: ' + object.y +' W: ' + object.w +' H: ' + object.h + ' object name: ' + object.name  + ' Collision Detected!'); 
-	}
-	my.handleLogic = function () {
-	}
-	my.draw = function (ctx, w, h) {
+        if (system.event.keymonitor.keyStates.right) {
+            my.xValue += 5;
+        }
+        if (system.event.keymonitor.keyStates.left) {
+            my.xValue -= 5;
+        }
+        if (system.event.keymonitor.keyStates.down) {
+            my.yValue += 5;
+        }
+        if (system.event.keymonitor.keyStates.up) {
+            my.yValue -= 5;
+        }
+        if (system.event.keymonitor.keyStates.spaceBar) {
+            console.log('spacebar pushed');
+        }
+        if (system.event.keymonitor.keyStates.enter) {
+            console.log('enter pushed');
+        }
 
-	    ctx.clearRect(0, 0, w, h);
-		ctx.fillStyle = "black"; 
-		ctx.fillRect(my.xValue, my.yValue, 100, 50);
-		ctx.fillStyle = "#222";
-		ctx.fillRect(0, 0, 64, h);
-		ctx.fillRect(w - 64, 0, 64, h);
-		ctx.fillStyle = "grey";
+    }
 
+    my.checkMouseCoordinates = function (event) {
+        //console.log('X: ' + event.pageX, 'Y: ' + event.pageY); 
+    }
+    my.clickHandler = function (object) {
+        console.log('X: ' + object.x + ' Y: ' + object.y + ' W: ' + object.w + ' H: ' + object.h + ' object name: ' + object.name + ' Collision Detected!');
+    }
+    my.handleLogic = function () {
+    }
+    my.draw = function (ctx, w, h) {
 
-		ctx.fillRect(16, (h / 2) - 64, 32, 32);
-		ctx.fillRect(16, (h / 2) - 32 + 6, 32, 32);
-		ctx.fillRect(16, (h / 2) - 0 + 12, 32, 32);
-
-		if (!my.once) {
-		    system.event.mousemonitor.subscribers.push({ x: 16, y: (h / 2) - 64, w: 32, h: 32, name: 'button1' });
-		    system.event.mousemonitor.subscribers.push({ x: 16, y: (h / 2) - 32  + 6, w: 32, h: 32, name: 'button2' });
-		    system.event.mousemonitor.subscribers.push({ x: 16, y: (h / 2) - 0 + 12, w: 32, h: 32, name: 'button3' });
-		    my.once = true;
-		}
+        ctx.clearRect(0, 0, w, h);
+        ctx.fillStyle = "black";
+        ctx.fillRect(my.xValue, my.yValue, 100, 50);
+        ctx.fillStyle = "#222";
+        ctx.fillRect(0, 0, 64, h);
+        ctx.fillRect(w - 64, 0, 64, h);
+        ctx.fillStyle = "grey";
 
 
+        ctx.fillRect(16, (h / 2) - 64, 32, 32);
+        ctx.fillRect(16, (h / 2) - 32 + 6, 32, 32);
+        ctx.fillRect(16, (h / 2) - 0 + 12, 32, 32);
 
-		ctx.fillRect(w - 48, (h / 2) - 64, 32, 32);
-		ctx.fillRect(w - 48, (h / 2) - 32 + 6, 32, 32);
-		ctx.fillRect(w- 48, (h / 2) - 0 + 12, 32, 32);
-		
+        if (!my.once) {
+            system.event.mousemonitor.subscribers.push({ x: 16, y: (h / 2) - 64, w: 32, z: 0, h: 32, name: 'button1' });
+            system.event.mousemonitor.subscribers.push({ x: 16, y: (h / 2) - 32 + 6, z: 1, w: 32, h: 32, name: 'button2' });
+            system.event.mousemonitor.subscribers.push({ x: 16, y: (h / 2) - 0 + 12, z: 3, w: 32, h: 32, name: 'button3', propagate:false });
+            system.event.mousemonitor.subscribers.push({ x: 32, y: (h / 2) - 0 + 12, z: 2, w: 32, h: 32, name: 'button4', propagate:false });
+            my.once = true;
+        }
 
-	}
+        ctx.fillStyle = "magenta";
+        ctx.fillRect(32, (h / 2) - 0 + 12, 32, 32);
 
-	return my;
+
+        ctx.fillRect(w - 48, (h / 2) - 64, 32, 32);
+        ctx.fillRect(w - 48, (h / 2) - 32 + 6, 32, 32);
+        ctx.fillRect(w - 48, (h / 2) - 0 + 12, 32, 32);
+
+
+    }
+
+    return my;
 }());
 
--- scripts/system.event.mousemonitor.js
+++ scripts/system.event.mousemonitor.js
@@ -4,41 +4,73 @@
         mouseY: 0
     };
 
-    my.subscribers = []; 
+    my.subscribers = [];
 
     my.checkMouseCoordinates = function (event) {
         my.mouseX = event.pageX;
-        my.mouseY = event.pageY; 
+        my.mouseY = event.pageY;
     }
     my.init = function () {
-    	addEventListener('mousemove', my.checkMouseCoordinates);
-    	addEventListener('click', my.checkMouseCoordinates);
-    	addEventListener('click', my.notifySubscribers);
+        addEventListener('mousemove', my.checkMouseCoordinates);
+        addEventListener('click', my.checkMouseCoordinates);
+        addEventListener('click', my.notifySubscribers);
     }
 
     my.notifySubscribers = function (event) {
-    	var x = event.pageX;
-    	var y = event.pageY;
-    	var w = 1;
-    	var h = 1;
+        var x = event.pageX;
+        var y = event.pageY;
+        var w = 1;
+        var h = 1;
 
-    	for (var i = 0; i < my.subscribers.length; i++){
-    		if(x < my.subscribers[i].x ){
-    			continue; 
-    		}
-    		if (x > my.subscribers[i].x + my.subscribers[i].w) {
-    			continue; 
-    		}
-    		if (y < my.subscribers[i].y) {
-    			continue; 
-    		}
-    		if (y > my.subscribers[i].y + my.subscribers[i].h) {
-    			continue;
-    		}
-    		// dispatch an event to state handler 
-    		game.state.statemanager.currentState.clickHandler(my.subscribers[i]); 
+        // sort subscribers into descending order to reflect how mouse interacts with objects 
+        my.subscribers.sort(function (a, b) {
+            var zA = 0, zB = 0;
+            if (a.z) {
+                zA = a.z;
+            }
+            if (b.z) {
+                zB = b.z;
+            }
 
-    	}
+            if (zA > zB) {
+                return -1;
+            }
+            if (zA < zB) {
+                return 1;
+            }
+            return 0;
+        });
+
+
+
+        for (var i = 0; i < my.subscribers.length; i++) {
+            if (x < my.subscribers[i].x) {
+                continue;
+            }
+            if (x > my.subscribers[i].x + my.subscribers[i].w) {
+                continue;
+            }
+            if (y < my.subscribers[i].y) {
+                continue;
+            }
+            if (y > my.subscribers[i].y + my.subscribers[i].h) {
+                continue;
+            }
+            // dispatch an event to state handler
+
+            
+            game.state.statemanager.currentState.clickHandler(my.subscribers[i]);
+
+            // if we don't want this event to propagate, stop notifying
+            if (my.subscribers[i].propagate != undefined && my.subscribers[i].propagate != true) {
+                break; 
+            }
+
+            //TODO: create a subscriber Z-order
+            //      order subscriber list 
+            //      add an optional Propagate flag to subscriber object  true == event keeps going, false == terminates notification
+
+        }
 
     }
 
@@ -47,7 +79,7 @@
         return {
             mouseX: my.mouseX,
             mouseY: my.mouseY
-        }; 
+        };
     }
 
     return my;
